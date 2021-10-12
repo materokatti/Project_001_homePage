775cf90f0bd5b79a9ede62588de6fef314d1ceeb
mateo <materokatti@gmail.com>
2021-10-12 16:04:13 +0900

    ADD: main finish - main animation finish - hero button layout finish - delete script - image setting

--- src/routes/components/Hero.svelte
+++ src/routes/components/Hero.svelte
@@ -1,22 +1,39 @@
-<script lang="ts">
-	import { fly } from 'svelte/transition';
-	import { quartInOut } from 'svelte/easing';
+<script context="module">
+	// export async function load({ page, fetch, session, context }) {
+	// 	const response = await fetch('$lib/images/beach.jpeg');
+	// 	const result = await response.json();
+	// 	const backgroundUrl = result;
+	// 	return {
+	// 		props: { backgroundUrl }
+	// 	};
+	// }
 </script>
 
-<section class="relative z-10" id="hero">
+<script>
+	// export let backgroundUrl;
+</script>
+
+<section class="hero relative z-10 bg-hero-back-to-t">
 	<div class="flex mx-0 my-auto pl-5 py-5 min-h-screen w-full items-center justify-start">
-		<div
-			in:fly={{ duration: 2000, x: 200, easing: quartInOut }}
-			out:fly={{ duration: 2000, x: 200, easing: quartInOut }}
-		>
-			<h1 in:fly={{ duration: 2000, x: 200, easing: quartInOut }}>Hello</h1>
-			<h1>My Name is</h1>
-			<h1>Doyun</h1>
+		<div>
+			<h1 class="animate-text_reveal_1 relative w-max text-transparent">
+				Hello
+				<span class="absolute top-0 left-0 h-full w-0 bg-green-300 animate-text_reaveal_box_1" />
+			</h1>
+			<h1 class="animate-text_reveal_2 relative w-max text-transparent">
+				My Name is<span
+					class="absolute top-0 left-0 h-full w-0 bg-green-300 animate-text_reaveal_box_2"
+				/>
+			</h1>
+			<h1 class="animate-text_reveal_name relative w-max text-transparent">
+				Doyun <span
+					class="absolute top-0 left-0 h-full w-0 bg-green-300 animate-text_reveal_box_3"
+				/>
+			</h1>
 			<a
 				href="/"
 				type="button"
-				class="border-2 border-green-300 px-4 mt-10 hover:bg-green-300 hover:text-gray-50"
-				>Portfolio</a
+				class="border-2 border-green-300 text-gray-50 px-4 mt-10 hover:bg-green-300 ">Portfolio</a
 			>
 		</div>
 	</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,53 @@ const config = {
 		container: {
 			center: true
 		},
-		extend: {}
+		extend: {
+			animation: {
+				text_reveal_1: 'text_reveal .5s ease .5s forwards',
+				text_reveal_2: 'text_reveal .5s ease 1.5s forwards',
+				text_reaveal_box_1: 'text_reveal_box 1s ease',
+				text_reaveal_box_2: 'text_reveal_box 1s ease 1s',
+				text_reveal_box_3: 'text_reveal_box 1s ease 2s',
+				text_reveal_name: 'text_reveal_name .5s ease 2.5s forwards'
+			},
+			keyframes: {
+				text_reveal_box: {
+					'50%': {
+						width: '100%',
+						left: '0'
+					},
+					'100%': {
+						width: '0',
+						left: '100%'
+					}
+				},
+				text_reveal: {
+					'100%': {
+						color: 'white'
+					}
+				},
+				text_reveal_name: {
+					'100%': {
+						color: '#6EE7B7',
+						fontweight: '500px'
+					}
+				}
+			},
+			backgroundImage: {
+				'hero-back': "url('src/lib/images/beach.jpeg')"
+			}
+		}
 	},
 	plugins: []
+};
+
+const colors = require('tailwindcss/colors');
+module.exports = {
+	theme: {
+		colors: {
+			green: colors.emerald
+		}
+	}
 };
 
 module.exports = config;
