067f83f29126aa57a80cb2406d514cb16862495c
mateo <materokatti@gmail.com>
2021-11-02 12:04:18 +0900

    ADD: project page start

---
+++ src/routes/components/Projects.svelte
@@ -0,0 +1,23 @@
+<section id="projects">
+	<div class="flex-col py-10 max-w-6xl container">
+		<h1 class="mb-10">Projects</h1>
+		<div class="flexBoxCenter flex-col">
+			<div class=" w-4/5 my-5 mx-auto">
+				<div class="project-info">
+					<h1><a href="./project01/project_page.html" target="_blank">Javascript</a></h1>
+					<img src="src/lib/images/JavaScript_logo.svg" alt="svelte" />
+				</div>
+			</div>
+			<!-- <div class="project-item">
+				<div class="project-info">
+					<h1><a href="./project02_CSS/project_page.html" target="blank">CSS Project</a></h1>
+					<h2>Paint in Code</h2>
+					<p>- Making layout with grid (with Poem of Neruda)</p>
+				</div>
+				<div class="project-img">
+					<img src="src/lib/images/JavaScript_logo.svg" alt="svelte" />
+				</div>
+			</div> -->
+		</div>
+	</div>
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,10 @@
 	import Nav from './components/Nav.svelte';
 	import Hero from './components/Hero.svelte';
 	import About from './components/About.svelte';
+	import Projects from './components/Projects.svelte';
 </script>
 
 <Nav />
 <Hero />
 <About />
+<Projects />
