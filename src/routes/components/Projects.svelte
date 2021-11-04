a18bd855d2cfce7c9bc34056978e1313225ec67c
mateo <materokatti@gmail.com>
2021-11-04 11:35:18 +0900

    ADD: project page toggle finish

--- src/routes/components/Projects.svelte
+++ src/routes/components/Projects.svelte
@@ -1,6 +1,7 @@
 <script>
 	import projectsData from '$lib/data/projectsData';
-	import { each } from 'svelte/internal';
+	import { slide } from 'svelte/transition';
+	import { quartInOut } from 'svelte/easing';
 	let projectInfoOn = 0;
 </script>
 
@@ -10,19 +11,25 @@
 		<div class="flexBoxCenter min-w-full flex-col">
 			{#each projectsData as project, index}
 				<div
-					class=" w-4/5 my-3 mx-auto"
+					class=" w-4/5 relative my-3 mx-auto"
 					on:click={() => {
 						projectInfoOn = projectInfoOn === project.id ? 0 : project.id;
 						console.log(project.projects, projectInfoOn);
 					}}
 				>
-					<div class="flex relative items-center justify-center bg-gray-100 p-3">
+					<div class="flex  items-center justify-center bg-gray-100 p-3">
 						<img class="absolute top-0 left-0 w-10" src={project.src} alt={project.name} />
 						<p>{project.name}</p>
 					</div>
 					{#if project.id === projectInfoOn}
-						<div class="block absolute z-10 bg-gray-100">
-							<h1>hi there</h1>
+						<div
+							class=" w-full absolute z-10 bg-gray-100 shadow-xl pb-3"
+							in:slide={{ duration: 300, easing: quartInOut }}
+							out:slide={{ duration: 300, easing: quartInOut }}
+						>
+							{#each project.projects as eachProject, index}
+								<p class="text-center text-base">{eachProject.title}</p>
+							{/each}
 						</div>
 					{/if}
 				</div>
